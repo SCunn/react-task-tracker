fea93ddd005485e1af8c7a7ce8518aa6e811b7f6
scunn <shanecunn@hotmail.com>
2021-04-19 19:25:40 +0100

    Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,15 @@
 
 import PropTypes from 'prop-types'
-<<<<<<< HEAD
 import Button from './Button'
-=======
->>>>>>> fe323e2a1cee94923aa786f1d16a094d0c660ad8
 
 
 const Header = ({ title }) => {
     return (
-<<<<<<< HEAD
         <header className='header'>
             <h1>{title}</h1>
             <Button color='green' text='Hello'/>
             <Button color='red' text='Hello 1'/>
             <Button color='orange' text='Hello 2'/>
-=======
-        <header>
-            <h1>{title}</h1>
->>>>>>> fe323e2a1cee94923aa786f1d16a094d0c660ad8
         </header>
     )
 }
