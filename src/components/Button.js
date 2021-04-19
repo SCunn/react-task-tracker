1654be3926f3c721185c94f5333b9d4af3f2bf75
scunn <shanecunn@hotmail.com>
2021-04-19 19:46:28 +0100

    onClick event in Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,29 @@
+import PropTypes from 'prop-types'
+
+
 const Button = ({ color, text }) => {
+    const onClick = () => {
+        console.log('click')
+    }
+
     return (
-        <button style={{ backgroundColor: color }} 
-        className='btn'>
+        <button
+        onClick={onClick} 
+        style={{ backgroundColor: color }} 
+        className='btn'
+        >
             {text}
-        </button>  
-        
+        </button>      
     )
+}
+
+Button.defaultProps = {
+    color: 'steelblue',
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,7 @@ const Header = ({ title }) => {
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Hello'/>
-            <Button color='red' text='Hello 1'/>
-            <Button color='orange' text='Hello 2'/>
+            <Button color='green' text='Add'/>
         </header>
     )
 }
