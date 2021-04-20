5a290387383a2df988214e4a62dfc74a7fe7650b
scunn <shanecunn@hotmail.com>
2021-04-20 17:46:34 +0100

    Added Tasks component, displayed task array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import React from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 const App = () => {
 
   return (
     <div className='container'>
       <Header />
+      <Tasks />
     </div>
   );
 }
