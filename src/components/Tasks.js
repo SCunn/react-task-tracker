e2cebb46b85e296bd90769af7a5e984465124f27
scunn <shanecunn@hotmail.com>
2021-04-21 00:39:29 +0100

    set tasks as global state

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,8 @@
-import { useState } from 'react'
 
 
 
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'School Appointment',
-            day: 'Feb 6th at 1:30pm',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Shopping',
-            day: 'Feb 7th at 2:30pm',
-            reminder: false,
-        },
-    ])
 
+const Tasks = ({ tasks }) => {
     return (
         <>
            {tasks.map((task) => (
