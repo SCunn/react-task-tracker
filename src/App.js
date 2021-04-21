3e173459cbf87212d1ac67d83efe85a8b36bb1aa
scunn <shanecunn@hotmail.com>
2021-04-21 19:13:29 +0100

    Build for production & JSON server

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 // import React from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import AddTask from './components/AddTask'
 
 const App = () => {
     const [showAddTask, setShowAddTask] = useState(false)
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'School Appointment',
-            day: 'Feb 6th at 1:30pm',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Shopping',
-            day: 'Feb 7th at 2:30pm',
-            reminder: false,
-        },
-    ])
+    const [tasks, setTasks] = useState([])
 
     // Add Task
     const addTask = (task) => {
