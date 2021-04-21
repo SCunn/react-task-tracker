70c03ce53e3a6fccd3f297f2350c727fee8c3403
scunn <shanecunn@hotmail.com>
2021-04-21 15:04:00 +0100

    Add Task Form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import { useState } from 'react'
+import AddTask from './components/AddTask'
 
 const App = () => {
       const [tasks, setTasks] = useState([
@@ -44,6 +45,7 @@ const App = () => {
   return (
     <div className='container'>
       <Header />
+      <AddTask />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} 
         onDelete={deleteTask}
