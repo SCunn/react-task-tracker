3a1b5325d31d0fa7a392efeb986c3cd75effe28a
scunn <shanecunn@hotmail.com>
2021-04-21 16:14:19 +0100

    show AddTask State

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useState } from 'react'
 import AddTask from './components/AddTask'
 
 const App = () => {
+    const [showAddTask, setShowAddTask] = useState(false)
       const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -52,7 +53,7 @@ const App = () => {
   return (
     <div className='container'>
       <Header />
-      <AddTask onAdd={addTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} 
         onDelete={deleteTask}
