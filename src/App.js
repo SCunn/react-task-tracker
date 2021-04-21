55154718b7685d96fb29c89f638d84afd5f34d55
scunn <shanecunn@hotmail.com>
2021-04-21 21:43:55 +0100

    Routing Footer & About

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 // import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
+import About from './components/About'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import { useState, useEffect } from 'react'
 import AddTask from './components/AddTask'
@@ -87,21 +90,30 @@ const App = () => {
     }
 
   return (
+    <Router>
     <div className='container'>
       <Header onAdd={() => setShowAddTask
         (!showAddTask)}
         showAdd={showAddTask}
         />
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} 
-        onDelete={deleteTask}
-        onToggle={toggleReminder}
-        />
-        ) : (
-          'No Tasks To Show'
-        )}
+
+        <Route path='/' exact render={(props) => (
+          <>
+            {showAddTask && <AddTask onAdd={addTask}/>}
+            {tasks.length > 0 ? (
+            <Tasks tasks={tasks} 
+            onDelete={deleteTask}
+            onToggle={toggleReminder}
+            />
+            ) : (
+            'No Tasks To Show'
+            )}
+          </>
+        )} />
+        <Route path='/about' component={About} />
+        <Footer />
     </div>
+    </Router>
   );
 }
 
